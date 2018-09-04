bc6245632f307478875b019204caed5aeab6f3a2
ccareyncsu <ccarey@ncsu.edu>
2018-09-04 14:12:17 -0600

    drills complete

--- DSA-Hashmaps/hashmapdrills.js
+++ DSA-Hashmaps/hashmapdrills.js
@@ -1,4 +1,5 @@
 'use strict';
+const LinkedList = require('./linked-list');
 
 class HashMap {
   constructor(initialCapacity=8) {
@@ -21,16 +22,27 @@ class HashMap {
     if (loadRatio > HashMap.MAX_LOAD_RATIO) {
       this._resize(this._capacity * HashMap.SIZE_RATIO);
     }
-
+    console.log(key);
     const index = this._findSlot(key);
     if (!this._slots[index]) {
+      this._slots[index] = new LinkedList();
+      this._slots[index].insertFirst({key, value});
       this.length++;
+    }else{
+      if(this._slots[index].find(key)){
+        this._slots[index].remove(key);
+      }
+      this._slots[index].insertLast({key, value});
     }
-    this._slots[index] = {
-      key,
-      value,
-      deleted: false
-    };
+
+
+
+
+    // this._slots[index] = {
+    //   key,
+    //   value,
+    //   deleted: false
+    // };
   }
 
   remove(key) {
@@ -39,22 +51,30 @@ class HashMap {
     if (slot === undefined) {
       throw new Error('Key error');
     }
-    slot.deleted = true;
-    this.length--;
-    this._deleted++;
+    slot.remove(key);
+    if(slot.head === null){
+      this.length--;
+      this._deleted++;
+    }
+
+    // slot.deleted = true;
+    // this.length--;
+    // this._deleted++;
   }
 
   _findSlot(key) {
     const hash = HashMap._hashString(key);
     const start = hash % this._capacity;
 
-    for (let i=start; i<start + this._capacity; i++) {
-      const index = i % this._capacity;
-      const slot = this._slots[index];
-      if (slot === undefined || (slot.key === key && !slot.deleted)) {
-        return index;
-      }
-    }
+
+    return start;
+    // for (let i=start; i<start + this._capacity; i++) {
+    //   const index = i % this._capacity;
+    //   const slot = this._slots[index];
+    //   if (slot === undefined || (slot.key === key && !slot.deleted)) {
+    //     return index;
+    //   }
+    // }
   }
 
   _resize(size) {
@@ -65,9 +85,15 @@ class HashMap {
     this._deleted = 0;
     this._slots = [];
 
+    console.log(oldSlots);
     for (const slot of oldSlots) {
       if (slot !== undefined && !slot.deleted) {
-        this.set(slot.key, slot.value);
+        let current = slot.head;
+        while(current !== null){
+          this.set(current.value.key, current.value.value);
+          current = current.next;
+        }
+        // this.set(slot.key, slot.value);
       }
     }
   }
@@ -88,23 +114,22 @@ HashMap.SIZE_RATIO = 3;
 const main = () => {
   let lotr = new HashMap();
 
-  // lotr.set('Hobbit', 'Bilbo');
-  // lotr.set('Hobbit', 'Frodo');
-  // lotr.set('Wizard', 'Gandolf');
-  // lotr.set('Human', 'Aragon');
-  // lotr.set('Elf', 'Legolas');
-  // lotr.set('Maiar', 'The Necromancer');
-  // lotr.set('Maiar', 'Sauron');
-  // lotr.set('RingBearer', 'Gollum');
-  // lotr.set('LadyOfLight', 'Galadriel');
-  // lotr.set('HalfElven', 'Arwen');
-  // lotr.set('Ent', 'Treebeard');
+  lotr.set('Hobbit', 'Bilbo');
+  lotr.set('Hobbit', 'Frodo');
+  lotr.set('Wizard', 'Gandolf');
+  lotr.set('Human', 'Aragon');
+  lotr.set('Elf', 'Legolas');
+  lotr.set('Maiar', 'The Necromancer');
+  lotr.set('Maiar', 'Sauron');
+  lotr.set('RingBearer', 'Gollum');
+  lotr.set('LadyOfLight', 'Galadriel');
+  lotr.set('HalfElven', 'Arwen');lotr.set('Ent','Treebeard');
 
   
 
-  // console.log((JSON.stringify(lotr, null, 2)));
+  console.log((JSON.stringify(lotr, null, 2)));
 
-  // console.log(lotr.get('Maiar'));
+  console.log(lotr.get('Maiar'));
 };
 
 main();
@@ -148,17 +173,30 @@ const palindrome = string => {
 //sort anagrams for same char push firstArr
 //push first into second
 
+
 const anagram = arr => {
   let anagrams = new HashMap();
   let firstArr = [];
   let secArr = [];
+
+  // for(var i = 0; words.length; i++){
+  //   var currentWord = word[i]; // cars
+  //   var sortedWord = sort(currentWord); // acrs
+  //   if(!anagrams[sortedWord]){
+  //     anagrams[sortedWord] = [currentWord];
+  //     //acrs: ['cars']
+  //   }else{
+  //     anagrams[sortedWord].push(currentWord);
+  //     //acrs: ['cars', 'arcs']
+  //   }
+  // }
 
   for (let i=0; i<arr.length; i++){
     let sorted = arr[i].split('').sort().join('');
     try{
       anagrams.set(sorted, [...anagrams.get(sorted), arr[i]]);
       // console.log('Try block', ...anagrams.get(sorted));
-      console.log('Arr[i]', arr[i]);
+      // console.log('Arr[i]', arr[i]);
     }catch{
       secArr.push(sorted);
       anagrams.set(sorted, [arr[i]]);
@@ -173,6 +211,6 @@ const anagram = arr => {
   return firstArr;
 }
 
-console.log(anagram(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']));
+// console.log(anagram(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']));
 
 
